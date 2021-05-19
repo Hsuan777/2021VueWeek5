2cf9c3919ea3648a469146ccbae246713d17d11d
Vic <Vic@gmail.com>
2021-05-19 15:37:26 +0800

    [ add ][ html, js]新增優惠券資料

--- js/admin.js
+++ js/admin.js
@@ -8,23 +8,25 @@ const App = Vue.createApp({
       hasLogin: false,
       displayData: {
         products: true,
-        orders: false
+        orders: false,
+        coupons: false
       },
       tabList: ['商品', '訂單', '優惠券', '文章'],
       selectTabName: '商品',
+      selectTabNameEn: 'product',
       originData: {
         products: [],
         orders: []
       },
       tempData: {
-        product: {}
+        product: {},
+        coupon: {}
       }
     };
   },
   methods: {
     getProduct(page = 1) {
       axios.get(`${this.url}/api/${this.path}/admin/products?page=${page}`).then(res => {
-        console.log(res.data.products);
         this.originData.products = res.data.products;
       }).catch(res => {
         console.log(res);
@@ -32,8 +34,12 @@ const App = Vue.createApp({
     },
     getOrder(page = 1) {
       axios.get(`${this.url}/api/${this.path}/admin/orders?page=${page}`,).then(res => {
-        console.log(res.data.orders);
         this.originData.orders = res.data.orders;
+      })
+    },
+    getCoupon(page = 1) {
+      axios.get(`${this.url}/api/${this.path}/admin/coupons?page=${page}`,).then(res => {
+        this.originData.coupons = res.data.coupons;
       })
     },
     addProduct() {
@@ -42,40 +48,73 @@ const App = Vue.createApp({
           ...this.tempData.product
         }
       }
-      console.log(productObj);
       axios.post(`${this.url}/api/${this.path}/admin/product`, productObj).then(res => {
-        console.log(res.data);
-        this.getProduct()
+        if (res.data.success) {
+          this.getProduct()
+        } else {
+          console.log(res.data.message);
+        }
+      })
+    },
+    addCoupon() {
+      let date = new Date();
+      let couponObj = {
+        data: {
+          ...this.tempData.coupon
+        }
+      }
+      const setDueDate = (num) => {
+        date.setDate(num)
+        couponObj.data['due_date'] = date.getTime();
+      }
+      setDueDate(couponObj.data['due_date']);
+      couponObj.data['is_enabled'] = 0
+      axios.post(`${this.url}/api/${this.path}/admin/coupon`, couponObj).then(res => {
+        if (res.data.success) {
+          this.getCoupon();
+        } else {
+          console.log(res.data.message);
+        }
       })
     },
     deleteProduct(itemId) {
       console.log(itemId);
       axios.delete(`${this.url}/api/${this.path}/admin/product/${itemId}`).then(res => {
         console.log(res.data);
-        this.getProduct()
+        this.getProduct();
       })
     },
     selectTab(item) {
-      // 點自己會錯亂
-      switch (item) {
-        case '商品':
-          this.selectTabName = item
-          this.displayData.products = !this.displayData.products
-          this.displayData.orders = !this.displayData.orders
-          this.getProduct()
-          break;
-        case '訂單':
-          this.selectTabName = item
-          this.displayData.products = !this.displayData.products
-          this.displayData.orders = !this.displayData.orders
-          this.getOrder()
-          break;
-        case '優惠券':
-          this.selectTabName = item
-          break;
-        case '文章':
-          this.selectTabName = item
-          break;
+      if (item !== this.selectTabName) {
+        switch (item) {
+          case '商品':
+            this.selectTabName = item
+            this.selectTabNameEn = 'product'
+            this.displayData.products = true
+            this.displayData.orders = false
+            this.displayData.coupons = false
+            this.getProduct()
+            break;
+          case '訂單':
+            this.selectTabName = item
+            this.displayData.products = false
+            this.displayData.orders = true
+            this.displayData.coupons = false
+            this.getOrder()
+            break;
+          case '優惠券':
+            this.selectTabName = item
+            this.selectTabNameEn = 'coupon'
+            this.displayData.products = false
+            this.displayData.orders = false
+            this.displayData.coupons = true
+            this.getCoupon()
+            break;
+          case '文章':
+            this.selectTabName = item
+            this.selectTabNameEn = 'article'
+            break;
+        }
       }
     },
     logOut() {
